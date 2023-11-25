63cfd631786edfa4d6d20422524a5a86f765b68c
Antonio Úbeda Montero <anubedam@gmail.com>
2023-11-25 22:09:51 +0100

    angular material added

--- src/app/base/base.module.ts
+++ src/app/base/base.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Material modules
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTableModule } from '@angular/material/table';
+
+const MATERIAL_MODULES = [
+  MatMenuModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatButtonModule,
+  MatIconModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatTableModule,
+];
+
 @NgModule({
   declarations: [],
   imports: [CommonModule],
+  exports: [...MATERIAL_MODULES],
 })
 export class BaseModule {}
