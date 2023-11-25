95d9280ac8f974ad2105b70216913526a69082ff
Antonio Úbeda Montero <anubedam@gmail.com>
2023-11-25 23:11:39 +0100

    incluido translate module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+      defaultLanguage: 'es',
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/form/form.module.ts
+++ src/app/form/form.module.ts
@@ -2,15 +2,32 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FormRoutingModule } from './form-routing.module';
-
 import { SharedModule } from '../shared/shared.module';
-
 import { FormComponent } from './pages/form.component';
+import {
+  TranslateCompiler,
+  TranslateLoader,
+  TranslateModule,
+  TranslateParser,
+} from '@ngx-translate/core';
+import { HttpLoaderFactory } from '../app.module';
+import { HttpClient } from '@angular/common/http';
 
 const COMPONENTS = [FormComponent];
 
 @NgModule({
   declarations: [...COMPONENTS],
-  imports: [CommonModule, FormRoutingModule, SharedModule],
+  imports: [
+    CommonModule,
+    FormRoutingModule,
+    SharedModule,
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
 })
 export class FormModule {}
